8b7bbf9ff96f3523194641be66ba0fe11e0c12b9
Marcely Santello <marcely@sbsoftware.com.br>
2020-11-14 00:06:07 -0300

    Other entities and second migration

---
+++ SalesWebMVC/Models/Enums/SaleStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        pending = 0,
+        billed = 1, 
+        canceled = 2
+    }
+}
---
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SalesWebMVC.Models.Enums;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public float Amount { get; set; }
+        public SaleStatus Status { get; set; }
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, float amount, SaleStatus status)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+        }
+    }
+}
